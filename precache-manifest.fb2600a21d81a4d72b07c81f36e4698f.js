96f922461cbcabbbbdd0206ad6a97ff438981e7a
paradocxstd <twerup@yandex.ru>
2019-04-07 15:02:55 +0300

    Updates

---
+++ precache-manifest.fb2600a21d81a4d72b07c81f36e4698f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkfarm/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "64db1ecab2125fa69f4e",
+    "url": "/vkfarm/static/js/runtime~main.64db1eca.js"
+  },
+  {
+    "revision": "71075b8bcc20298dbcf5",
+    "url": "/vkfarm/static/js/main.71075b8b.chunk.js"
+  },
+  {
+    "revision": "c69ef10c400182063a35",
+    "url": "/vkfarm/static/js/2.c69ef10c.chunk.js"
+  },
+  {
+    "revision": "71075b8bcc20298dbcf5",
+    "url": "/vkfarm/static/css/main.f03f2eaf.chunk.css"
+  },
+  {
+    "revision": "c69ef10c400182063a35",
+    "url": "/vkfarm/static/css/2.7a1a4758.chunk.css"
+  },
+  {
+    "revision": "92d62652dde249de57272c36a29a8493",
+    "url": "/vkfarm/index.html"
+  }
+];
